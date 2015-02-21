d78f55b49b21910710d80cb1ecefaee0b2ebeecf
Ben Bertka <bbertka@users.noreply.github.com>
2015-02-21 12:38:07 -0800

    Update gdun.rb
    Now using square logos hosted on imgur

--- jobs/gdun.rb
+++ jobs/gdun.rb
@@ -5,16 +5,16 @@ require 'json'
  def Gdun.random_gdun
   gduns =[
   ['081466849','Microsoft','http://i.imgur.com/8MuBOXp.jpg'],
-  ['155366107','Starbucks','http://i.imgur.com/RO5vgMj.png'],
-  ['009256819','Boeing','http://i.imgur.com/m1FPr1h.jpg'],
-  ['092180517','Expedia','http://i.imgur.com/HRLzfZZ.jpg'],
-  ['103391843','Costco','http://i.imgur.com/QM9VL8V.jpg'],
-  ['048341267','Paccar','http://i.imgur.com/Z5OVd5c.jpg'],
-  ['831703157','PSE','http://i.imgur.com/Ij2jv7O.jpg'],
-  ['884727413','Providence Health','http://i.imgur.com/BMpRvNJ.jpg'],
+  ['155366107','Starbucks','http://i.imgur.com/CRmvA1o.jpg'],
+  ['009256819','Boeing','http://i.imgur.com/vtlZIjT.jpg'],
+  ['092180517','Expedia','http://i.imgur.com/XfVdApY.png'],
+  ['103391843','Costco','http://i.imgur.com/g6afQml.png'],
+  ['048341267','Paccar','http://i.imgur.com/VsLQEoP.jpg'],
+  ['831703157','PSE','http://i.imgur.com/CCktnis.jpg'],
+  ['884727413','Providence Health','http://i.imgur.com/FcFl9TL.jpg'],
   ['327376653','T-Mobile','http://i.imgur.com/hZKFuvp.png'],
-  ['047897855','Intel','http://i.imgur.com/uxixdRc.png'],
-  ['007947146','Russell Investments','http://i.imgur.com/Sh5e38o.jpg']
+  ['047897855','Intel','http://i.imgur.com/iM9fgVZ.jpg'],
+  ['007947146','Russell Investments','http://i.imgur.com/bpmX0XF.jpg']
   ]
   return gduns.sample
  end
